ef46a9acd7bcfc78272738a3dcda6c70a33c4484
Grinko1 <86093873+Grinko1@users.noreply.github.com>
2023-05-15 09:50:04 +0900

    fixed dropping selection onClick delete button ( add stopPropagation), returned  single quote

--- src/app.js
+++ src/app.js
@@ -10,27 +10,32 @@ import './styles.css';
 function App({ store }) {
   const list = store.getState().list;
 
+  const handleDelete = (code, e) => {
+    e.stopPropagation();
+    store.deleteItem(code);
+  };
+
   return (
-    <div className="App">
-      <div className="App-head">
+    <div className='App'>
+      <div className='App-head'>
         <h1>Приложение на чистом JS</h1>
       </div>
-      <div className="App-controls">
+      <div className='App-controls'>
         <button onClick={() => store.addItem()}>Добавить</button>
       </div>
-      <div className="App-center">
-        <div className="List">
+      <div className='App-center'>
+        <div className='List'>
           {list.map((item) => (
-            <div key={item.code} className="List-item">
+            <div key={item.code} className='List-item'>
               <div
                 className={'Item' + (item.selected ? ' Item_selected' : '')}
                 onClick={() => store.selectItem(item.code)}>
-                <div className="Item-code">{item.code}</div>
-                <div className="Item-title">
+                <div className='Item-code'>{item.code}</div>
+                <div className='Item-title'>
                   {item.title} {item.amountSelect > 0 ? declension(item.amountSelect) : ''}
                 </div>
-                <div className="Item-actions">
-                  <button onClick={() => store.deleteItem(item.code)}>Удалить</button>
+                <div className='Item-actions'>
+                  <button onClick={(e) => handleDelete(item.code, e)}>Удалить</button>
                 </div>
               </div>
             </div>
